caaaed72cc049126d40f439b6d2b4dd9293daa81
DungeonMaster3000 <igorobed@yandex.ru>
2023-11-21 20:59:47 +0300

    Add base logging

--- train.py
+++ train.py
@@ -1,25 +1,44 @@
 from os import listdir
 import logging
+import torch
 from utils import make_runs_subdir
 
 
 def train():
+    # задаем параметры общего логгера
+    # будет еще один логгер, который в поддирректорию будет записывать результаты обучения
+    # на каждой эпохе
+    logging.basicConfig(
+        level=logging.INFO,
+        filename="file_logs.log",
+        filemode="w",
+        format="%(asctime)s %(levelname)s %(message)s"
+        )
+    logging.info("Train start")
+    
     # 0. для текущего эксперимента создать подпапку в папке runs
-    make_runs_subdir(mode="val")
-    # 1. Проверить, что в папке data лежит датасет и два txt файла train и val
-
-    #try:
-    #    data_dir_files = listdir("./data/Caltech256/")
-    #    if "train.txt" not in data_dir_files or "val.txt" not in data_dir_files:
-    #        raise FileNotFoundError
-    #except FileNotFoundError:
+    try:
+        results_dir = make_runs_subdir(mode="train")
+    except Exception:
+        logging.error("Error when creating a subdirectory")
+        return
+    # логгирование успеха или неудачии создания дирректории для результатов обучения
+    logging.info(f"Created subdirrectory for results - {results_dir}")
+    
+    # 1. Проверить, что в папке data лежит датасет и два файла: train_lst.txt и val_lst.txt
+    try:
+        data_dir_files = listdir("./data/Caltech256/")
+        if "train_lst.txt" not in data_dir_files or "val_lst.txt" not in data_dir_files:
+            raise FileNotFoundError
+    except FileNotFoundError:
         # здесь нужно логгировать ошибку
-    #    pass
+        logging.error("Incorrect dataset - FileNotFoundError")
+        return
+    logging.info("Caltech256 dataset exists in folder data")
 
-    
-    
-    # здесь нужно логгировать успешную проверку, что датасет лежит внутри папки
-    # 2. 
+    # 2.Установим device
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    logging.info(f"Device type - {device}")
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -1,20 +1,22 @@
 from os import listdir, mkdir
 
 
-def make_runs_subdir(mode="train"):
+def make_runs_subdir(mode: str = "train") -> str:
     """
     Функция создает подпапку, в которой будут храниться
     результаты обучения/валидации
     """
     runs_files = listdir("./runs/")
-    run_subdirs = [item for item in runs_files if "." not in item and "run_" in item]
+    run_subdirs = [item for item in runs_files if "." not in item and mode in item]
     if len(run_subdirs) == 0:
-        subdir_name = "run_0"    
+        subdir_name = f"run_{mode}_0"
     else:
         max_num = max([int(item.split("_")[-1]) for item in run_subdirs])
-        subdir_name = "run_" + str(max_num + 1)
+        subdir_name = f"run_{mode}_{str(max_num + 1)}"
     
     mkdir("./runs/" + subdir_name)
     # создадим также подпапку для весов, если mode=="train"
     if mode == "train":
         mkdir("./runs/" + subdir_name + "/weights")
+
+    return subdir_name
