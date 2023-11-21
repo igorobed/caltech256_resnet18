c5864ce6f5e457e8af1e5a7fe10206e720a4823c
DungeonMaster3000 <igorobed@yandex.ru>
2023-11-21 04:35:54 +0300

    Add runs folder

--- train.py
+++ train.py
@@ -1,6 +1,14 @@
+from os import listdir
+import logging
+
+
 def train():
     # 1. Проверить, что в папке data лежит датасет и два txt файла train и val
-    pass
+    try:
+        listdir("./data/Caltech256/")
+    except FileNotFoundError:
+        # здесь нужно логгировать ошибку
+    # здесь нужно логгировать успешную проверку, что датасет лежит внутри папки
     # 2. 
 
 
